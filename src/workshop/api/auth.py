657bb3f62feed9d9757ce1d62d8d479f531f5164
dimajazz <dimazuh@gmail.com>
2021-09-17 19:42:22 +0300

    auth second step

--- src/workshop/api/auth.py
+++ src/workshop/api/auth.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter
 from fastapi.param_functions import Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from models.auth import UserCreate, Token
+from models.auth import UserCreate, Token, User
+from services.auth import AuthService, get_current_user
 
 
 router = APIRouter(
@@ -11,10 +12,24 @@ router = APIRouter(
 
 
 @router.post('/sign-up', response_model=Token)
-def sign_up(user_data: UserCreate):
-	pass
+def sign_up(
+	user_data: UserCreate, 
+	service: AuthService = Depends(),
+	):
+	return service.register_new_user(user_data)
 
 
 @router.post('/sign-in', response_model=Token)
-def sign_in(form_data: OAuth2PasswordRequestForm = Depends()):
-	pass
+def sign_in(
+	form_data: OAuth2PasswordRequestForm = Depends(),
+	service: AuthService = Depends(),
+	):
+	return service.authenticate_user(
+		form_data.username,
+		form_data.password,
+	)
+
+
+@router.get('/user', response_model=User)
+def get_user(user: User = Depends(get_current_user)):
+	return user
--- src/workshop/settings.py
+++ src/workshop/settings.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
 	
 	jwt_secret: str
 	jwt_algorithm: str = 'HS256'
+	jwt_expiration: int = 3600
 
 
 settings = Settings(
